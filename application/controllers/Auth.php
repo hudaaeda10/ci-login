03c367e361a0a3fbfa0d4b6e0fcf7ebcabfd985a
Huda Aeda <hudaaeda10@gmail.com>
2021-03-09 17:33:37 +0700

    Pertemuan #3 - Membuat Halaman Registration

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,17 +3,47 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Auth extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+    }
+
     public function index()
     {
-        $this->load->view('templates/auth_header');
+        $data['title'] = 'Login Page';
+        $this->load->view('templates/auth_header', $data);
         $this->load->view('auth/login');
         $this->load->view('templates/auth_footer');
     }
 
     public function registration()
     {
-        $this->load->view('templates/auth_header');
-        $this->load->view('auth/registration');
-        $this->load->view('templates/auth_footer');
+        $data['title'] = 'CI Login Website';
+        // rule form validation
+        $this->form_validation->set_rules('name', 'Name', 'required|trim');                         //trim agar spasi di awal dan di akhir tidak masuk database
+        $this->form_validation->set_rules('email', 'Email', 'required|valid_email|trim|is_unique[user.email]');
+        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]', ['matches' => 'Password dont matches!', 'min_length' => 'Password too sort!']);
+        $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
+        // form validation
+        if ($this->form_validation->run() ==  FALSE) {
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/registration');
+            $this->load->view('templates/auth_footer');
+        } else {
+            $data = [
+                'name' => htmlspecialchars($this->input->post('name')),
+                'email' => htmlspecialchars($this->input->post('email')),
+                'image' => 'default.jpg',
+                'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
+                'role_id' => 2,
+                'is_activated' => 1,
+                'date_created' => time()
+            ];
+
+            $this->db->insert('user', $data);
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert"> Congratulation! Your account has been created</div>');
+            redirect('auth');
+        }
     }
 }
