0e71ab89ca24bea50beb8ab873d7cb804e51c5c3
Huda Aeda <hudaaeda10@gmail.com>
2021-03-17 18:49:56 +0700

    Pertemuan #11 - Change Password

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -68,4 +68,44 @@ class User extends CI_Controller
             redirect('user');
         }
     }
+
+    public function changepassword()
+    {
+        $data['title'] = 'Change Password';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+
+        $this->form_validation->set_rules('current_password', 'Current Password', 'required|trim');
+        $this->form_validation->set_rules('new_password1', 'New Password', 'required|trim|min_length[3]|matches[new_password2]');
+        $this->form_validation->set_rules('new_password2', 'Confirm New Password', 'required|trim|min_length[3]|matches[new_password1]');
+
+        if ($this->form_validation->run() == FALSE) {
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/sidebar', $data);
+            $this->load->view('templates/topbar', $data);
+            $this->load->view('user/changepassword', $data);
+            $this->load->view('templates/footer');
+        } else {
+            $current_password = $this->input->post('current_password');
+            $new_password = $this->input->post('new_password1');
+            // memeriksa current password dengan database
+            if (!password_verify($current_password, $data['user']['password'])) {
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Curent Password Wrong!</div>');
+                redirect('user/changepassword');
+            } else {
+                // jika current password sama dengan password baru maka error
+                if ($current_password == $new_password) {
+                    $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Curent Password cannot be same as new password</div>');
+                    redirect('user/changepassword');
+                } else {
+                    // jika password ok semua
+                    $password_hash = password_hash($new_password, PASSWORD_DEFAULT);
+                    $this->db->set('password', $password_hash);
+                    $this->db->where('email', $this->session->userdata('email'));
+                    $this->db->update('user');
+                    $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Password Has Been Change!</div>');
+                    redirect('user/changepassword');
+                }
+            }
+        }
+    }
 }
--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -2,7 +2,13 @@
 <div class="container-fluid">
 
     <!-- Page Heading -->
-    <h1 class="h3 mb-4 text-gray-800">Dashboard</h1>
+    <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h1 class="display-4">Welcome to home page CI Login</h1>
+            <p class="lead">This is a training to make website CI Login </p>
+        </div>
+    </div>
 </div>
 <!-- /.container-fluid -->
 
